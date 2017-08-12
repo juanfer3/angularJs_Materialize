49ecbd55db257666470f73172badd5bf6810b3bf
juanfer3 <jfvilladiego2@misena.edu.co>
2017-08-12 18:25:03 -0500

    insercion de usuarios tablas

--- rutas.js
+++ rutas.js
@@ -1,83 +1,75 @@
-var app= angular.module('myApp',['ngRoute']);
-
-app.config(function($routeProvider) {
-    
-    $routeProvider.when('/',{
-    templateUrl: 'Fragmentos/Home.html'
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'Fragmentos/Home.html'
     })
-	.when('/Home',{
-    templateUrl: 'Fragmentos/Home.html'
+        .when('/Home', {
+        templateUrl: 'Fragmentos/Home.html'
     })
-	.when('/Listar',{
-    templateUrl: 'Fragmentos/Listar.html'
+        .when('/Listar', {
+        templateUrl: 'Fragmentos/Listar.html'
     })
-	.when('/Editar',{
-    templateUrl: 'Fragmentos/Editar.html'
+        .when('/Editar', {
+        templateUrl: 'Fragmentos/Editar.html'
     })
-	.when('/Ver',{
-    templateUrl: 'Fragmentos/Ver.html'
+        .when('/Ver', {
+        templateUrl: 'Fragmentos/Ver.html'
     })
-	.when('/Crear',
-    {
-    templateUrl: 'Fragmentos/Crear.html'
+        .when('/Crear', {
+        templateUrl: 'Fragmentos/Crear.html'
     });
-
-       
+});
+app.controller('CrearController', function ($scope, $http) {
+    $scope.us;
+    $scope.cedula;
+    $scope.rol;
+    $scope.user;
+    $scope.contrasena;
+ 
+	$scope.id = "1";
+    $scope.x;
+    $scope.tomo;
+    $scope.con = function () {
+        $http({
+            method: 'POST',
+            url: 'node_modules/modelo/prueba.php'
+        }).then(function (response) {
+            this.tomo = response.data;
+            console.log(response.data);
+            alert(tomo);
+            $scope.x = this.tomo;
+        }, function (error) {
+            alert("no");
+        });
+    };
     
-});
-
-app.controller('CrearController', function($scope, $http){
-	$scope.nombre;
-	$scope.cedula;
-	$scope.rol;
-	$scope.user;
-	$scope.contrasena;
-
-	$scope.x;
-	$scope.tomo;
-	
-	$scope.con = function(){
-		
-		
-	$http({
-      method: 'GET',
-      url: 'node_modules/modelo/prueba.php'
-   }).then(function (response){
-		this.tomo=response.data;
-		console.log(response.data);
-		alert(tomo);
-		$scope.x=this.tomo;
-   },function (error){
-		alert("no");
-   });
-		
-		
-	}
-	
-	$scope.enviar=function(){
-		
-		alert(this.nombre);
-		alert(this.cedula);
-		alert(this.rol);
+	$scope.insert = function () {
+         $scope.data = {
+            'metodo': "POST",
+            'usuario': $scope.user,
+			'contrasena':$scope.contrasena,
+    		'rol':$scope.rol
+    
+		};
+		$scope.metodo="soy el metodo post";
 		alert(this.user);
-		alert(this.contrasena);
-	}
-
+		$http({
+            method: 'POST',
+            data:this.data,
+      	   url:'node_modules/Controller/InsertarEmpleado.php'
+        }).then(function (respose) {
+//            alert(respose.data);
+//            console.log(respose.config);
+//            console.log(respose);
+        }, function (error) {
+            alert(":(");
+        });
+    };
+    $scope.enviar = function () {
+        alert(this.nombre);
+        alert(this.cedula);
+        alert(this.rol);
+        alert(this.user);
+        alert(this.contrasena);
+    };
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
