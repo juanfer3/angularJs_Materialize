2f44b69c3524b18c7655c71219e6c0c558e39b33
juanfer3 <jfvilladiego2@misena.edu.co>
2017-08-16 19:45:23 -0500

    nested form

--- funciones.js
+++ funciones.js
@@ -1,5 +1,63 @@
-function  boton(){
-	
-	alert("hola");
-	
-}
+
+app.controller('CrearController', function ($route, $scope, $http, $routeParams) {
+    $scope.us;
+    $scope.cedula;
+    $scope.rol;
+    $scope.user;
+    $scope.contrasena;
+    $scope.users;
+
+    $scope.id = "1";
+    $scope.x;
+    $scope.tomo;
+
+    $scope.con = function () {
+        $http({
+            method: 'POST',
+            url: 'node_modules/modelo/prueba.php'
+        }).then(function (response) {
+            this.tomo = response.data;
+            console.log(response.data);
+            alert(tomo);
+            $scope.x = this.tomo;
+        }, function (error) {
+            alert("no");
+        });
+    };
+
+    $scope.insert = function () {
+        $scope.data = {
+            'metodo': "POST",
+            'nombre':$scope.nombre,
+            'cedula':$scope.cedula,
+            'usuario': $scope.user,
+            'contrasena': $scope.contrasena,
+            'rol': $scope.rol
+
+        };
+        $scope.metodo = "soy el metodo post";
+        alert(this.user);
+        $http({
+            method: 'POST',
+            data: this.data,
+            url: 'node_modules/Controller/InsertarEmpleados.php'
+        }).then(function (respose) {
+            alert(respose.data);
+            console.log(respose.config);
+            console.log(respose);
+        }, function (error) {
+            alert(":(");
+        });
+    };
+    $scope.getUsers = function () {
+        $http({
+            method: 'POST',
+            url: "node_modules/Controller/ListarEmpleados.php"
+        }).then(function (response) {
+            console.log(response.data);
+            $scope.users = response.data;
+        }), function (falla) {
+            alert("falla en la conexion");
+        };
+    };
+});
--- rutas.js
+++ rutas.js
@@ -7,7 +7,8 @@ app.config(function ($routeProvider) {
         templateUrl: 'Fragmentos/Home.html'
     })
         .when('/Listar', {
-        templateUrl: 'Fragmentos/Listar.html'
+        templateUrl: 'Fragmentos/Listar.html',
+        controller: 'CrearController'
     })
         .when('/Editar', {
         templateUrl: 'Fragmentos/Editar.html'
@@ -19,57 +20,66 @@ app.config(function ($routeProvider) {
         templateUrl: 'Fragmentos/Crear.html'
     });
 });
-app.controller('CrearController', function ($scope, $http) {
-    $scope.us;
-    $scope.cedula;
-    $scope.rol;
-    $scope.user;
-    $scope.contrasena;
- 
-	$scope.id = "1";
-    $scope.x;
-    $scope.tomo;
-    $scope.con = function () {
-        $http({
-            method: 'POST',
-            url: 'node_modules/modelo/prueba.php'
-        }).then(function (response) {
-            this.tomo = response.data;
-            console.log(response.data);
-            alert(tomo);
-            $scope.x = this.tomo;
-        }, function (error) {
-            alert("no");
-        });
-    };
-    
-	$scope.insert = function () {
-         $scope.data = {
-            'metodo': "POST",
-            'usuario': $scope.user,
-			'contrasena':$scope.contrasena,
-    		'rol':$scope.rol
-    
-		};
-		$scope.metodo="soy el metodo post";
-		alert(this.user);
-		$http({
-            method: 'POST',
-            data:this.data,
-      	   url:'node_modules/Controller/InsertarEmpleado.php'
-        }).then(function (respose) {
+
+
+
+//app.controller('CrearController', function ($route,$scope,$http,$routeParams){
+//    $scope.us;
+//    $scope.cedula;
+//    $scope.rol;
+//    $scope.user;
+//    $scope.contrasena;
+//	$scope.users;
+// 
+//	$scope.id = "1";
+//    $scope.x;
+//    $scope.tomo;
+//	
+//    $scope.con = function () {
+//        $http({
+//            method: 'POST',
+//            url: 'node_modules/modelo/prueba.php'
+//        }).then(function (response) {
+//            this.tomo = response.data;
+//            console.log(response.data);
+//            alert(tomo);
+//            $scope.x = this.tomo;
+//        }, function (error) {
+//            alert("no");
+//        });
+//    };
+//    
+//	$scope.insert = function () {
+//         $scope.data = {
+//            'metodo': "POST",
+//            'usuario': $scope.user,
+//			'contrasena':$scope.contrasena,
+//    		'rol':$scope.rol
+//    
+//		};
+//		$scope.metodo="soy el metodo post";
+//		alert(this.user);
+//		$http({
+//            method: 'POST',
+//            data:this.data,
+//      	   url:'node_modules/Controller/InsertarEmpleado.php'
+//        }).then(function (respose) {
 //            alert(respose.data);
 //            console.log(respose.config);
-//            console.log(respose);
-        }, function (error) {
-            alert(":(");
-        });
-    };
-    $scope.enviar = function () {
-        alert(this.nombre);
-        alert(this.cedula);
-        alert(this.rol);
-        alert(this.user);
-        alert(this.contrasena);
-    };
-});
+//           console.log(respose);
+//        }, function (error) {
+//            alert(":(");
+//        });
+//    };
+//           $http({
+//			method: 'POST',
+//			url:"node_modules/Controller/ListarEmpleados.php"
+//		}).then(function(response){
+//			console.log(response.data);
+//			$scope.users=response.data;
+//		}),function(falla){
+//			alert("falla en la conexion");
+//		};
+//   
+//});
+
